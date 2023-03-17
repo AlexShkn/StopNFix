140224129de440605ee1611a9406bbfa852ed416
AlexShkn <barsuk-b@yandex.ru>
2023-03-17 18:12:58 +0300

    страница contacts +адаптив

--- src/js/app.js
+++ src/js/app.js
@@ -110,3 +110,66 @@ function isWebp() {
 isWebp()
 
 //====================================================================
+
+const phoneFiled = document.getElementById('phone-field')
+let keyCode
+
+filedMask(phoneFiled)
+
+function filedMask(input) {
+	function mask(event) {
+		event.keyCode && (keyCode = event.keyCode)
+		let pos = this.selectionStart
+		if (pos < 3) event.preventDefault()
+		let matrix = '+7 (___) ___-__-__',
+			i = 0,
+			def = matrix.replace(/\D/g, ''),
+			val = this.value.replace(/\D/g, ''),
+			newValue = matrix.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+			})
+		i = newValue.indexOf('_')
+		if (i != -1) {
+			i < 5 && (i = 3)
+			newValue = newValue.slice(0, i)
+		}
+		let reg = matrix
+			.substr(0, this.value.length)
+			.replace(/_+/g, function (a) {
+				return '\\d{1,' + a.length + '}'
+			})
+			.replace(/[+()]/g, '\\$&')
+		reg = new RegExp('^' + reg + '$')
+		if (
+			!reg.test(this.value) ||
+			this.value.length < 5 ||
+			(keyCode > 47 && keyCode < 58)
+		)
+			this.value = newValue
+		if (event.type == 'blur' && this.value.length < 5) this.value = ''
+	}
+
+	input.addEventListener('input', mask, false)
+	input.addEventListener('focus', mask, false)
+	input.addEventListener('blur', mask, false)
+	input.addEventListener('keydown', mask, false)
+}
+
+//====================================================================
+const navLinks = document.querySelectorAll('.menu__link')
+const locateName = window.location.pathname.replace('/', '')
+
+navLinks.forEach(link => {
+	link.classList.remove('_current')
+
+	if (locateName === link.getAttribute('href')) {
+		link.classList.add('_current')
+	}
+})
+//====================================================================
+const header = document.querySelector('.header')
+if (window.location.pathname !== '/index.html') {
+	header.classList.add('header-shadow')
+} else {
+	header.classList.remove('header-shadow')
+}
